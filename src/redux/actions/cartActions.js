c3dccd71284fa21b2e45c93b0eaf9b3c8a00b65a
ak177 <abhaykansal177@gmail.com>
2022-08-26 21:13:13 +0530

    backend links updated

--- src/redux/actions/cartActions.js
+++ src/redux/actions/cartActions.js
@@ -2,8 +2,8 @@ import * as actionTypes from "../constants/cartConstants";
 import axios from "axios";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-  // const { data } = await axios.get(`/api/products/${id}`);
-  const { data } = await axios.get(`https://artshaala.herokuapp.com/api/products/${id}`);
+  const { data } = await axios.get(`/api/products/${id}`);
+  // const { data } = await axios.get(`https://artshaala.herokuapp.com/api/products/${id}`);
 
   dispatch({
     type: actionTypes.ADD_TO_CART,
